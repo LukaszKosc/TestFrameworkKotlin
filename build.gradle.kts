6cf16d655ccd406857bcd3a525aff154cc623f47
Lukasz <lukasz@email.com>
2023-01-24 00:16:41 +0100

    Working solution - without saving to file

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,28 +10,15 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
-    flatDir {
-        dirs("libs")
-    }
 }
 sourceSets{
     val main by getting
     val test by getting
-    val all by creating{
-        java {
-            srcDir(arrayOf("src/main/kotlin", "src/test/kotlin"))
-            compileClasspath += main.output + test.output
-        }
-    }
-
 }
 dependencies {
-
     implementation("org.testng:testng:7.7.0")
     implementation("org.apache.maven.plugins:maven-surefire-plugin:3.0.0-M7")
     implementation("org.jetbrains.kotlin:kotlin-reflect:1.8.0")
-    runtimeOnly(":TestFramework:1.0-SNAPSHOT")
-//    implementation(":tests.local")
 }
 
 tasks.test {
@@ -40,21 +27,6 @@ tasks.test {
         showStandardStreams = true
     }
     useTestNG() {
-        doFirst {
-            // gradle testClasses //compiles tests but do not run them!!!
-//            classpath = files("$buildDir/classes/kotlin/main", "$buildDir/classes/kotlin/test")
-//            println("classpath: $classpath")
-//            classpath.forEach { if (it.toString().contains("\\test")) println(it) }
-        }
-//        val args = []
-//        if(project.hasProperty("defaultListeners")) {
-//            args += ["-usedefaultlisteners", "true"]
-//        } else {
-//            args += ["-usedefaultlisteners", "false",
-//                "-listener", "testng.listeners.TestExecListener"]
-//        }
-
-
         testLogging.showStandardStreams = true
 
         val parallelValue = properties["parallel"]
@@ -69,7 +41,6 @@ tasks.test {
         }
 
         val tags = properties["tags"]
-//        println("tags: '$tags'.")
         if (tags != null) {
             includeGroups("$tags")
         }
@@ -90,73 +61,24 @@ application {
 }
 
 tasks.register<Jar>("testJar") {
-//    archiveFileName.set("appTest-${project.version}.jar")
+    archiveFileName.set("appTest-${project.version}.jar")
     from(sourceSets.test.get().output)
-    manifest {
-        attributes(mapOf("Main-Class" to "local.test.framework.HelperKt",
-            "Implementation-Title" to "Gradle Test Classes",
-            "Implementation-Version" to "1.0-SNAPSHOT"))
-    }
-    // To avoid the duplicate handling strategy error
-    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
-
-    // To add all of the dependencies
-    from(sourceSets.main.get().output)
-
-    dependsOn(configurations.runtimeClasspath)
-//    dependsOn(configurations.compileClasspath)
-    from({
-        configurations.runtimeClasspath.get().filter { it.name.endsWith("jar") }.map { zipTree(it) }
-    })
 }
 
-//
 tasks.register<Jar>("appJar") {
 //    archiveFileName.set("appMain-${project.version}.jar")
+    //sufficient appJar jar generation - line 'from(sourceSets.test.get().output)' provides classes from 'src/test/kotlin'
     from(sourceSets.main.get().output)
     manifest {
         attributes(mapOf("Main-Class" to "MainKt",
             "Implementation-Title" to "Gradle App Classes",
             "Implementation-Version" to "1.0-SNAPSHOT"))
     }
-
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+    from(sourceSets.test.get().output)
     dependsOn(configurations.runtimeClasspath)
+    dependsOn(configurations.compileClasspath)
     from({
         configurations.runtimeClasspath.get().filter { it.name.endsWith("jar") }.map { zipTree(it) }
     })
 }
-
-//tasks.register<Jar>("testJar") {
-////    from(sourceSets.main.get().output)
-//    from("kotlin/tests/local/"){
-//        include("**")
-//    }
-//}
-
-//tasks.jar {
-//    from(sourceSets["test"].output)
-//}
-
-//
-//tasks.create("args_eater"){
-//    println("hello from: '"+this.name+"'.")
-//    val songName = properties["tags"]
-//        ?: System.getenv("TAGS")
-//        ?: error("""Property "tags" or environment variable "TAGS" not found""")
-//    println("Value: $songName")
-//    doLast{
-//        println("test msg")
-//        if (project.hasProperty("args")) {
-//            println("Our input argument with project property ["+project.name+"]")
-//        }
-//
-//    }
-//}
-//
-//task("getTags"){
-//    println("hello from: '"+this.name+"'.")
-//    val songName = properties["tags"]
-//        ?: System.getenv("TAGS")
-//        ?: error("""Property "tags" or environment variable "TAGS" not found""")
-//    println("Value: $songName")
-//}
--- src/main/kotlin/local/test/framework/Runner.kt
+++ src/main/kotlin/local/test/framework/Runner.kt
@@ -1,102 +1,222 @@
 package local.test.framework
 
+import org.apache.commons.io.FileUtils
 import org.codehaus.classworlds.Launcher
+
 import org.testng.TestNG
+import org.testng.annotations.Test
 import org.testng.xml.XmlClass
 import org.testng.xml.XmlSuite
 import org.testng.xml.XmlTest
 import java.io.File
+import java.io.IOException
 import java.net.URL
+import java.util.*
+import java.util.jar.JarEntry
+import java.util.jar.JarFile
+import java.util.logging.Logger
+import kotlin.reflect.full.declaredMembers
+import kotlin.reflect.full.findAnnotation
+import kotlin.reflect.full.hasAnnotation
+
+var logger: Logger = Logger.getLogger("Runner")
 
 open class Runner {
     companion object {
+
         fun runTestNG() {
-            var cp = System.getProperty("java.class.path")
-//            println("before: $cp")
-            cp += ";C:\\Users\\Kostek\\IdeaProjects\\TestFramework\\build\\classes\\kotlin\\test"
-            System.setProperty("java.class.path", cp)
-//            println("after '${System.getProperty("java.class.path")}'.")
+//            var cp = System.getProperty("java.class.path")
+////            println("before: $cp")
+//            cp += ";C:\\Users\\Kostek\\IdeaProjects\\TestFramework\\build\\classes\\kotlin\\test"
+//            System.setProperty("java.class.path", cp)
+////            println("after '${System.getProperty("java.class.path")}'.")
+//            println(System.getProperty("user.dir"))
+            logger!!.info { "This is my first log4j's statement" }
+            val targetDir = ".\\"
+            unpackJar(
+                "C:\\Users\\Kostek\\IdeaProjects\\TestFramework\\libs\\TestFramework-1.0-SNAPSHOT.jar",
+                targetDir
+            )
+            val classes = getClassesDescription()
+            removeLocalFiles()
 
-//            findClasses("local.test.framework.test")
-            println(System.getProperty("user.dir"))
+            classes.forEach { logger.info {"got class: '${it}'"} }
 
-            var myTestNG: TestNG = TestNG()
-            var mySuite: XmlSuite = XmlSuite()
-
-            myTestNG.setUseDefaultListeners(false)
-            mySuite.setName("Sample Suite")
-//            mySuite.addListener("local.local.test.TestListener");
+//            var myTestNG: TestNG = TestNG()
+//            var mySuite: XmlSuite = XmlSuite()
+//
+//            myTestNG.setUseDefaultListeners(false)
+//            mySuite.setName("Sample Suite")
+////            mySuite.addListener("local.local.test.TestListener");
 ////            mySuite.addListener("org.uncommons.reportng.JUnitXMLReporter");
-            var myTest: XmlTest = XmlTest(mySuite);
-            myTest.setName("Sample Test");
-            val myClasses: MutableList<XmlClass> = ArrayList<XmlClass>().toMutableList()
-//            myClasses += XmlClass("local.test.framework.test.TestClass")
-            myClasses += XmlClass("local.test.framework.tests.TestClass")
-            myClasses += XmlClass("local.test.framework.tests.TestClass2")
-
-
-
-            myTest.xmlClasses = myClasses;
-            var myTests: MutableList<XmlTest> = ArrayList<XmlTest>().toMutableList()
-            myTests.add(myTest);
-            mySuite.setTests(myTests);
-            var mySuites: MutableList<XmlSuite> = ArrayList<XmlSuite>().toMutableList()
-            mySuites.add(mySuite);
-            myTestNG.setXmlSuites(mySuites);
-            myTestNG.run();
-
-
-//            //working code
-//            var x: Class<*>? = null
-//            lateinit var clazzX: Class<*>
-//            val tla = TestListenerAdapter()
-//            val testng = TestNG()
-//            try{
-//                x = Class.forName("local.test.framework.test.TestClass")
-//            } catch (e: ClassNotFoundException) {
-//                // it does not exist on the classpath
-//                println("err: $e")
-//            }
-//            testng.setTestClasses(arrayOf(local.test.framework.test.TestClass::class.java))
-//            testng.addListener(tla)
-//            testng.run()
-
+//            var myTest: XmlTest = XmlTest(mySuite);
+//            myTest.setName("Sample Test");
+//            val myClasses: MutableList<XmlClass> = ArrayList<XmlClass>().toMutableList()
+//            myClasses += XmlClass("local.test.framework.tests.TestClass")
+//            myClasses += XmlClass("local.test.framework.tests.TestClass2")
+//
+//            myTest.xmlClasses = myClasses;
+//            var myTests: MutableList<XmlTest> = ArrayList<XmlTest>().toMutableList()
+//            myTests.add(myTest);
+//            mySuite.setTests(myTests);
+//            var mySuites: MutableList<XmlSuite> = ArrayList<XmlSuite>().toMutableList()
+//            mySuites.add(mySuite);
+//            myTestNG.setXmlSuites(mySuites);
+//            myTestNG.run();
         }
 
+        fun unpackJar(jarFilePath: String, targetDir: String) {
+            try {
+                val userDir = File(targetDir)
+                userDir.mkdir()
+                val jar = JarFile(jarFilePath)
+                jar.stream()
+                    .filter { file: JarEntry ->
+                        file.name.contains("local/test/framework/tests/")
+                    }
+                    .peek { file: JarEntry ->
+                        val res =
+                            File(userDir.path + "/" + file.name)
+                        if (file.isDirectory) {
+                            res.mkdirs()
+                        } else {
+                            try {
+                                FileUtils.copyInputStreamToFile(jar.getInputStream(file), res)
+                            } catch (e: IOException) {
+                                println(e.message)
+                            }
+                        }
+                    }.forEach { file: JarEntry -> logger.info { "Extracted file: " + file.name } }
+            } catch (e: IOException) {
+                throw Exception(
+                    java.lang.String.format(
+                        "Unable to open jar %s", jarFilePath
+                    ), e
+                )
+            }
+        }
+
+        fun removeLocalFiles() {
+            val directory = File("local\\")
+
+            if (directory.exists()) {
+                directory.deleteRecursively()
+            }
+            if (!directory.exists()) {
+                logger.info { "Directory '$directory' deleted." }
+
+            }
+        }
+
+        fun findClasses(pckgname: String): List<Object> {
+            var classesList: MutableList<Object> = mutableListOf()
+//            println("package: $pckgname")
+            // Translate the package name into an absolute path
+            var name = pckgname
+            if (!name.startsWith("/")) {
+                name = "/$name"
+            }
+            name = name.replace('.', '/')
+//            println("name: $name")
+            // Get a File object for the package
+//            val url: URL = Launcher::class.java.getResource(name)
+//            val directory = File(url.getFile())
+            val directory = File("./$name")
+
+//            println("Finding classes:")
+            if (directory.exists()) {
+//                println("directory exists")
+                // Get the list of the files contained in the package
+                directory.walk()
+                    .filter { f -> f.isFile() && f.name.contains('$') == false && f.name.endsWith(".class") }
+                    .forEach {
+//                        println("pckgname: $pckgname")
+                        val fullyQualifiedClassName = pckgname +
+                                it.canonicalPath.removePrefix(directory.canonicalPath)
+                                    .dropLast(6) // remove .class
+                                    .replace('/', '.')
+                                    .replace('\\', '.')
+                        try {
+                            // Try to create an instance of the object
+//                            println("fullyQualifiedClassName: $fullyQualifiedClassName")
+                            val o = Class.forName(fullyQualifiedClassName).getDeclaredConstructor().newInstance()
+                            if (o is Object) {
+                                classesList += o
+                            }
+                        } catch (cnfex: ClassNotFoundException) {
+                            System.err.println(cnfex)
+                        } catch (iex: InstantiationException) {
+                            // We try to instantiate an interface
+                            // or an object that does not have a
+                            // default constructor
+                        } catch (iaex: IllegalAccessException) {
+                            // The class is not public
+                        }
+                    }
+            } else {
+                println("no such path")
+            }
+            return classesList
+        }
+
+        fun getClassesDescription(clazzList: List<Object> = findClasses("local.test.framework.tests")): List<Object> {
+            val clazzesList: MutableList<Map<String, Map<String, String>>> =
+                mutableListOf<Map<String, Map<String, String>>>().toMutableList()
+            var clazzDesc: MutableList<Map<String, MutableList<Map<String, String>>>> =
+                mutableListOf<Map<String, MutableList<Map<String, String>>>>().toMutableList()
+            clazzList.forEach {
+
+                val className = it.`class`.name
+
+//                println("className: $className")
+                if (className.contains("TestClass")) {
+//                    println("className: $className")
+
+                    var myListOfMethods: MutableList<Map<String, String>> = mutableListOf()
+//                    clazzesList += clazzDesc
+                    it::class.declaredMembers.forEach { method ->
+                        run {
+//                        clazzDesc[className] = clazzDesc[className]!! + mapOf("method" to method.name)
+//                            println("method: ${method.name}")
+//                            clazzDesc.add(mutableMapOf("className" to className))
 
 
-//        fun listCodeClasses() {
-//            val reflections = Reflections("my.project.prefix")
-//
-//            val allClasses: Set<Class<out Any?>> = reflections.getSubTypesOf(Any::class.java)
-//
-//
-//            val pkg: String = TabA::class.java.getPackage().getName()
-//            val relPath = pkg.replace('.', '/')
-//
-//            val resource = ClassLoader.getSystemClassLoader().getResource(relPath)
-//                ?: throw RuntimeException(
-//                    "Unexpected problem: No resource for "
-//                            + relPath
-//                )
-//
-//            val f = File(resource.path)
-//
-//            val files = f.list()
-//
-//            for (i in files.indices) {
-//                val fileName = files[i]
-//                var className: String? = null
-//                var fileNm: String? = null
-//                if (fileName.endsWith(".class")) {
-//                    fileNm = fileName.substring(0, fileName.length - 6)
-//                    className = "$pkg.$fileNm"
-//                }
-//                if (className != null) {
-//                    if (!tabClasses.containsKey(className)) tabClasses.put(fileNm, className)
-//                }
-//            }
-//        }
+                            if (method.hasAnnotation<Test>()) {
+                                val testAnnotation = method.findAnnotation<Test>()
+//                                println("testAnnotation: $testAnnotation")
+                                if (null != testAnnotation) {
+                                    val groups: Array<String> = testAnnotation.groups
+                                    var groupsList = ""
+                                    if (groups != null) {
+                                        if (groups.isNotEmpty()) {
+                                            groups.forEach { group ->
+                                                groupsList += "$group,"
+//                                                println("group: $group")
+                                            }
+                                        }
+                                    }
+                                    groupsList = groupsList.dropLast(1)
+                                    myListOfMethods.add(mapOf("method" to method.name, "groups" to groupsList))
+                                }
+                            }
+                        }
+
+//                    clazzesList += clazzDesc
+                    }
+                    clazzDesc.add(
+                        mapOf(
+                            className to myListOfMethods
+                        )
+                    )
+                }
+            }
+            clazzDesc.forEach {
+                run {
+                    logger.info { "got element: ${it.entries}" }
+                }
+            }
+        return clazzList
+        }
     }
 
 }
